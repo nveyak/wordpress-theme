f6e1cdc2df140899941ab39224e53cc87f9bd62c
Dean Kramer <deanjkramer@gmail.com>
2013-06-26 21:26:34 -0700

    test

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp-nveyak');
+define('DB_NAME', 'nveyakcom_wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'deanohyeahcom');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'seventy1');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.deanohyeah.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
