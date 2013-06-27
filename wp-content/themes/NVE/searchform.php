abd11220cd979c5ff840a8d57ebc79187e1b824e
Dean Kramer <deanjkramer@gmail.com>
2013-06-27 00:38:50 -0700

    ignore files

--- wp-content/themes/NVE/searchform.php
+++
@@ -1,3 +0,0 @@
-<form class="navbar-search" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<input type="text" value="" name="s" id="s" class="search-query" placeholder="search">
-</form>
